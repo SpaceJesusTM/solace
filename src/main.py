3764b97c7c7243aea95ff567cbdbd3d0fa888e34
SpaceJesusTM <sugihikoruo@gmail.com>
2022-11-06 00:06:12 -0400

    fixed typos in main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import discord
 import asyncio
 from discord.ext import commands
-from cogs import discussion, announcments, channels, mail
+from cogs import discussion, announcements, channels, mail
 # from cogs
 
 bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
@@ -9,7 +9,7 @@ bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
 @bot.event
 async def on_ready():
     await bot.add_cog(discussion.discussion(bot))
-    await bot.add_cog(announcments.announcments(bot))
+    await bot.add_cog(announcements.announcments(bot))
     await bot.add_cog(channels.channels(bot))
     await bot.add_cog(mail.mail(bot))
     print('Bot is ready!')
